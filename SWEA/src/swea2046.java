f48a40ea74d1dd2dc6910ffb1640e197985b32bd
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-05-09 00:14:25 +0900

    2046 : 스탬프 찍기(반복문)
    주어진 숫자만큼 # 을 출력하기.
    
    - 똑같은 문자를 반복해서 출력할 땐 조건문과 반복문 사용
    for(초기화;조건식;증감식;)

---
+++ SWEA/src/swea2046.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class swea2046 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next(); //toUpperCase() 소문자를 대분자로 바꾸기, toLowerCase() 대문자를 소문자로 바꾸기.
+	
+		System.out.println(input.toUpperCase()); 
+	}
+}
