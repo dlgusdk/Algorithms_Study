af81a578be3ff0d52052e00452746c96a3a8bac1
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-07-18 18:11:23 +0900

    .

---
+++ Backjoon/src/달팽이_2869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ¥ﬁ∆ÿ¿Ã_2869 {
+	public static void main(String[] args) {
+		System.out.println("V, A, B");
+		Scanner sc = new Scanner(System.in);
+		int A= sc.nextInt();
+		int B= sc.nextInt();
+		int V= sc.nextInt();
+		
+		System.out.printf("%d", V - (A - B));
+
+		
+	}
+
+}
